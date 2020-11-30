0f1257910cbbb22927048a12dd78fd4038bf56f2
Stephan <stephan.sinn.edu@gmail.com>
2020-11-30 23:04:51 +0100

    education update

--- src/views/projects/Education.js
+++ src/views/projects/Education.js
@@ -7,8 +7,24 @@ class Education extends React.Component {
       <Container fluid>
         <h1><i className="fas fa-graduation-cap" /> Education and Training</h1>
           <p>
-            The DataScore Project offers training for educating students at all stages. Furthermore, we offer education guidelines and recommendations for repository operators.
-            There is more to come soon.
+            The DataScore Project offers guidelines and recommendations to stress the importance of research data management already in the graduate and post graduate programs.
+          </p>
+          <p>
+            Primary recommendations:
+            <ul>
+              <li>
+                Integrate RDM in the curriculum, for example in (laboratory) internships and field work
+              </li>
+              <li>
+                Include RDM as a separate item in the doctoral agreements
+              </li>
+              <li>
+                Integrate RDM in the assessment of bachelor, master and doctoral theses
+              </li>
+            </ul>
+          </p>
+          <p>
+            Furthermore, we offer a set of recommendations and “best practice guides” for repository operators to enhance the analyzability of dataset metrics.
           </p>
           <p>
             For any questions please contact: <a href="mailto:manuel.tsotsalas@kit.edu">Dr. Manuel Tsotsalas</a> or
